8f98d30d097d3a7a7b5a25ad48b464f3295d684b
scrdoctor1412 <pp19helldog@gmail.com>
2025-03-30 22:34:08 +0700

    working on login screen

--- englens/lib/src/ui/screens/login/login_screen.dart
+++ englens/lib/src/ui/screens/login/login_screen.dart
@@ -11,17 +11,95 @@ class LoginScreen extends StatelessWidget {
     return GetBuilder<LoginScreenViewmodel>(
       init: LoginScreenViewmodel(),
       builder: (controller) {
+        controller.context = context;
         _body() {
-          return Column(
-            children: [
-              Text('New login screen'),
-              Image.asset('assets/images/test.jpg'),
-            ],
+          return Form(
+            key: controller.formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('New login screen'),
+                // Image.asset('assets/images/test.jpg'),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter email';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Color(0xff868686),
+                    ),
+                    label: Text('Email'),
+                    hintText: 'Enter email',
+                    // filled: true,
+                    // fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                      ),
+                      borderRadius: BorderRadius.circular(26),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter password';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock_outline_rounded,
+                      color: Color(0xff868686),
+                    ),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye_sharp,
+                      color: Color(0xff868686),
+                    ),
+                    label: Text('Password'),
+                    hintText: 'Enter email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(26),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    onPressed: () {
+                      controller.onLogin();
+                    },
+                    child: Text(
+                      'Signin',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           );
         }
 
         return Scaffold(
-          body: _body(),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: _body(),
+          ),
         );
       },
     );
--- englens/lib/src/ui/screens/login/login_screen_viewmodel.dart
+++ englens/lib/src/ui/screens/login/login_screen_viewmodel.dart
@@ -1,3 +1,17 @@
 import 'package:englens/src/core/base_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class LoginScreenViewmodel extends GetViewModelBase {}
+class LoginScreenViewmodel extends GetViewModelBase {
+  final formKey = GlobalKey<FormState>();
+
+  onLogin() {
+    if (formKey.currentState!.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+      ScaffoldMessenger.of(context!).showSnackBar(
+        SnackBar(content: Text('Processing Data')),
+      );
+    }
+  }
+}
